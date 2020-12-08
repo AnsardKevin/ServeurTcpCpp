17b762a5a521002ab197b1692052ef428861928d
ansard_k <ansard_k@etna-alternance.net>
2020-12-08 23:04:36 +0100

    push correction with C++ class

--- Serveur.cpp
+++ Serveur.cpp
@@ -10,11 +10,23 @@
 #include <signal.h>
 #include <iostream>
 
+#include "Serveur.hpp"
 
 #define MAX 80 
 #define PORT 8080 
 #define SA struct sockaddr 
-  
+
+
+Serveur::Serveur() {
+    this.sockfd = 0;
+    this.connfd = 0;
+
+    // assign IP, PORT 
+    this.servaddr.sin_addr.s_addr = inet_addr("0.0.0.0"); 
+    this.servaddr.sin_family = AF_INET; 
+    this.servaddr.sin_port = htons(PORT); 
+}
+
 // Function designed for chat between client and server. 
 void func(int sockfd) 
 { 
@@ -44,31 +56,22 @@ void func(int sockfd)
             break; 
         } 
     } 
-} 
-  
-// Driver function
-int main() 
-{ 
-    int sockfd = 0;
-    int connfd = 0;
-    socklen_t len = 0; 
-    struct sockaddr_in servaddr; 
-    struct sockaddr_in cli;
-    // socket create and verification 
-    sockfd = socket(AF_INET, SOCK_STREAM, 0); 
-    if (sockfd == -1) { 
+}
+
+
+int Serveur::Connection() {
+    socklen_t len = 0;
+    this.sockfd = socket(AF_INET, SOCK_STREAM, 0); 
+
+    if (this.sockfd == -1) { 
         printf("socket creation failed...\n"); 
         exit(0); 
     } 
     else
         printf("Socket successfully created..\n"); 
-    // assign IP, PORT 
-    servaddr.sin_addr.s_addr = inet_addr("0.0.0.0"); 
-    servaddr.sin_family = AF_INET; 
-    servaddr.sin_port = htons(PORT); 
   
     // Binding newly created socket to given IP and verification 
-    if ((bind(sockfd, (struct sockaddr *)&servaddr, sizeof(servaddr))) != 0) { 
+    if ((bind(this.sockfd, (struct sockaddr *)&this.servaddr, sizeof(this.servaddr))) != 0) { 
         printf("socket bind failed...\n"); 
         exit(0); 
     } 
@@ -76,18 +79,18 @@ int main()
         printf("Socket successfully binded..\n"); 
   
     // Now server is ready to listen and verification 
-    if ((listen(sockfd, 5)) != 0) { 
+    if ((listen(this.sockfd, 5)) != 0) { 
         printf("Listen failed...\n"); 
         exit(0); 
     } 
     else
         printf("Server listening..\n"); 
-    len = sizeof(cli); 
+    len = sizeof(this.cli); 
   
     // Accept the data packet from client and verification 
-    connfd = accept(sockfd, (struct sockaddr *)&cli, &len);
+    this.connfd = accept(this.sockfd, (struct sockaddr *)&this.cli, &len);
 //    connfd = accept(sockfd, (SA*)&cli, &len); 
-    if (connfd < 0) { 
+    if (this.connfd < 0) { 
         printf("server acccept failed...\n"); 
         exit(0); 
     } 
@@ -95,8 +98,17 @@ int main()
         printf("server acccept the client...\n"); 
   
     // Function for chatting between client and server 
-    func(connfd); 
+    func(this.connfd); 
   
     // After chatting close the socket 
-    close(sockfd); 
+    close(this.sockfd);
+    return 0;
 } 
+  
+// Driver function
+int main() 
+{
+    Serveur serv = new Serveur();
+    serv.Connection();
+    return 0;
+}
---
+++ Serveur.hpp
@@ -0,0 +1,27 @@
+#ifndef __SERVEUR_HH__
+#define __SERVEUR_HH__
+
+#include <stdio.h> 
+#include <netdb.h> 
+#include <unistd.h>
+#include <netinet/in.h> 
+#include <stdlib.h> 
+#include <string.h> 
+#include <sys/socket.h> 
+#include <sys/types.h> 
+#include <arpa/inet.h>
+#include <signal.h>
+#include <iostream>
+
+class Serveur {
+    private:
+        int sockfd;
+        int connfd;
+        struct sockaddr_in servaddr;
+        struct sockaddr_in cli;
+    public:
+        Serveur();
+        int Connection();
+};
+
+#endif __SERVEUR_HH__
